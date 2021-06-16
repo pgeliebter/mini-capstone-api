e5758155f6d05fdadcb699a8e962bf79b0655cad
Pinny Geliebter <pgibralter@gmail.com>
2021-06-16 14:07:11 -0400

    adds more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "Cherry Coke Zero", price: 1.75, image_url: "https://us.coca-cola.com/content/dam/nagbrands/us/coke/en/products/coca-cola-flavors/cherry/zero-sugar/cherry-zero-sugar_12oz_desktop.jpg", description: "The most awesomest soda of all time, every time")
+Product.create(name: "Cherry Coke Zero", price: 1.75, image_url: "https://us.coca-cola.com/content/dam/nagbrands/us/coke/en/products/coca-cola-flavors/cherry/zero-sugar/cherry-zero-sugar_12oz_desktop.jpg", description: "The most awesomest soda of all time, every time.")
+Product.create(name: "Aqua Lung Calypso Regulator", price: 250, image_url: "https://us.aqualung.com/dw/image/v2/BDTB_PRD/on/demandware.static/-/Sites-aqualung-m-catalog/default/dwe5dd1c64/images/products/rg126111_1.jpg?sw=765&sh=765", description: "Something to keep you alive underwater. Buy it or else the consenquences will be dire.")
