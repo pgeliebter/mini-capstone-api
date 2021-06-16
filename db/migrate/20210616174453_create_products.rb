cff417537b74ab5e791a7d44538f11eae7ffad22
Pinny Geliebter <pgibralter@gmail.com>
2021-06-16 13:47:03 -0400

    adds controller

---
+++ db/migrate/20210616174453_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.primary_key :id
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
