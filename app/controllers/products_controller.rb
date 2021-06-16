9ccdf047855f7d558d871be621b178925ac8c1a1
Pinny Geliebter <pgibralter@gmail.com>
2021-06-16 14:50:47 -0400

    adds 1 product path

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def get_all_products
     render json: Product.all.as_json
   end
+
+  def get_1_product
+    render json: Product.first.as_json
+  end
 end
