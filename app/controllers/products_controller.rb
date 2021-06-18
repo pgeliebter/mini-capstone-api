6492c70748f9b2c95f99b8b9b3fe8a95a38335bd
Pinny Geliebter <pgibralter@gmail.com>
2021-06-18 14:19:28 -0400

    adds patch route for Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,4 +19,14 @@ class ProductsController < ApplicationController
     product.save
     render json: product
   end
+
+  def update
+    product = Product.find(params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    product.save
+    render json: product
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
 end
