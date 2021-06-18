ff7a78eb19a0c2d8441cf4e248084840b8c1ca8c
Pinny Geliebter <pgibralter@gmail.com>
2021-06-18 15:12:14 -0400

    changes seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Product.create(name: "Cherry Coke Zero", price: 1.75, image_url: "https://us.coca-cola.com/content/dam/nagbrands/us/coke/en/products/coca-cola-flavors/cherry/zero-sugar/cherry-zero-sugar_12oz_desktop.jpg", description: "The most awesomest soda of all time, every time.")
-Product.create(name: "Aqua Lung Calypso Regulator", price: 250, image_url: "https://us.aqualung.com/dw/image/v2/BDTB_PRD/on/demandware.static/-/Sites-aqualung-m-catalog/default/dwe5dd1c64/images/products/rg126111_1.jpg?sw=765&sh=765", description: "Something to keep you alive underwater. Buy it or else the consenquences will be dire.")
+i = 0
+90000.times do
+  p i
+  i += 1
+  Product.create(name: "Cherry Coke Zero", price: 1.75, image_url: "https://us.coca-cola.com/content/dam/nagbrands/us/coke/en/products/coca-cola-flavors/cherry/zero-sugar/cherry-zero-sugar_12oz_desktop.jpg", description: "The most awesomest soda of all time, every time.")
+  # Product.create(name: "Aqua Lung Calypso Regulator", price: 250, image_url: "https://us.aqualung.com/dw/image/v2/BDTB_PRD/on/demandware.static/-/Sites-aqualung-m-catalog/default/dwe5dd1c64/images/products/rg126111_1.jpg?sw=765&sh=765", description: "Something to keep you alive underwater. Buy it or else the consenquences will be dire.")
+end
+# #2.times do
+#   Product.create(name: FFaker::Product.unique.name, price: rand(10000), image_url: FFaker::Image.unique.url, description: FFaker::AWS.unique.product_description)
+# end
