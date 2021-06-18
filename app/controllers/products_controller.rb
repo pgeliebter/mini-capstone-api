12b84694e9606edb3fb8592851c6911c1d2ff173
Pinny Geliebter <pgibralter@gmail.com>
2021-06-18 13:33:49 -0400

    changes some routes to conform with RESTful convention

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,21 +8,4 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id])
     render json: product
   end
-
-  # def get_product_by_params
-  #   #this method takes an id that is input through params and then redirects the user to render_params below
-  #   inputs = params["input"]
-  #   product = Product.find_by(id: inputs)
-
-  #   if inputs != nil
-  #     redirect_to "/product/#{product.name}/#{product.id}"
-  #   else
-  #     render json: { message: "product not found" }
-  #   end
-  # end
-
-  # def render_params
-  #   #this method and related route renders the product with the name in the url. there is a problem though that once redirected if the user changes the id it keeps the old product.name
-  #   render json: Product.find_by(id: params[:id])
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
-  # get "/1_product", controller: "products", action: "get_1_product"
-  # get "/product/:input" => "products#get_product_by_params"
-  # get "/product" => "products#get_product_by_params"
-  # get "/product/:name/:id" => "products#render_params"
 end
