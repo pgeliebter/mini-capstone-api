3b458ecc26b81ace834c95dd6a2ef81e805862f0
Pinny Geliebter <pgibralter@gmail.com>
2021-06-21 15:00:35 -0400

    adds some Product methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,11 @@ class Product < ApplicationRecord
   end
 
   def tax
-    @tax_rate = 0.09
-    return tax = price * @tax_rate
+    tax_rate = 0.09
+    tax = price * tax_rate
   end
 
   def total
-    return price * (1 + @tax_rate)
+    price * (1 + tax)
   end
 end
