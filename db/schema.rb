d4dc433ab79fd9f59dfa84f03e11a12e55da61e7
Pinny Geliebter <pgibralter@gmail.com>
2021-06-22 14:52:55 -0400

    adds inventory:integer to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_184910) do
+ActiveRecord::Schema.define(version: 2021_06_22_185214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_06_22_184910) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "inventory"
   end
 
 end
