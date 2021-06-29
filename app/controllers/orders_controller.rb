0bf27e34fea33246c8ce48dbf12e3771fdb33883
Pinny Geliebter <pgibralter@gmail.com>
2021-06-29 14:33:35 -0400

    adds show/index/create routes/methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,41 @@
 class OrdersController < ApplicationController
   def create
+    product = Product.find(params[:product_id])
     order = Order.new(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
     )
     if order.save
       render json: order
     else
       render json: order.errors
+    end
+  end
+
+  def show
+    order = Order.find(params[:id])
+
+    if current_user
+      if order
+        render json: order
+      else
+        render json: { error: "that order can't be found" }
+      end
+    else
+      render status: 401
+    end
+  end
+
+  def index
+    orders = Order.all
+    if current_user
+      render json: orders
+    else
+      render status: 401
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   post "/orders" => "orders#create"
+  get "/orders/:id" => "orders#show"
+  get "/orders" => "orders#index"
 end
