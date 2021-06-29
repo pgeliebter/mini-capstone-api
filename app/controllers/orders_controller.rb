849b9b099a7f76067dbd7f9404aec3a0ccf695e3
Pinny Geliebter <pgibralter@gmail.com>
2021-06-29 13:37:19 -0400

    adds initial test route/method for creating orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,9 @@
 class OrdersController < ApplicationController
+  def create
+    order = Order.new(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+    )
+  end
 end
