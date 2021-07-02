2bfb09d35ae2ee1dc1a38cf0cbb67c47e7d51348
Pinny Geliebter <pgibralter@gmail.com>
2021-07-02 13:42:27 -0400

    adds has_many through associations for product <> category models

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :orders
   has_many :product_categories
+  has_many :categories, through: :product_categories
   # validates :description, presence: true
 
   # def supplier
