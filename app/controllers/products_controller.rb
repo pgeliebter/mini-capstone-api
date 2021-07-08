3e71a5e51792bef79475c212bdf02e0c8d09beee
Pinny Geliebter <pgibralter@gmail.com>
2021-07-08 16:52:08 -0400

    removes product image_url params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,7 +21,6 @@ class ProductsController < ApplicationController
     product = Product.new(
       name: params["name"],
       price: params[:price],
-      image_url: params[:image_url],
       description: params[:description],
     )
     if product.save
@@ -35,7 +34,6 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id])
     product.name = params[:name] || product.name
     product.price = params[:price] || product.price
-    product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
     product.supplier_id = params[:supplier_id] || product.supplier_id
     if product.save
