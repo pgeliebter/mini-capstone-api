942864e68e96987ed1c282d0fc0452708a3b927c
Pinny Geliebter <pgibralter@gmail.com>
2021-07-19 14:36:32 -0400

    allows localhost:8080 connections

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com"
+    origins "mini-capstone.peterxjang.com", "localhost:8080"
 
     resource "*",
       headers: :any,
